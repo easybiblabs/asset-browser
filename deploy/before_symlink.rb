f4d1b83949249cf80c3a74b81242ad1e3c5b8921
Florian Holzhauer <fh-github@fholzhauer.de>
2014-07-25 13:50:19 +0200

    Removed prefer-source

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -3,7 +3,6 @@ composer_command << " #{release_path}/composer.phar"
 composer_command << " --no-interaction"
 composer_command << " install"
 composer_command << " --no-dev"
-composer_command << " --prefer-source"
 composer_command << " --optimize-autoloader"
 
 run "cd #{release_path} && #{composer_command}"
